40d2ce5d1371afa9ad17d256ec507191dd44e11e
Mons <ah.mounsef@gmail.com>
2025-08-08 23:48:19 +0200

    Setup Project

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
     return (
-        <h1 className="text-3xl font-bold underline">Hello world</h1>
+        <h1 className="text-3xl font-bold underline">Tailwind Landing Page</h1>
     )
 }
 export default App
